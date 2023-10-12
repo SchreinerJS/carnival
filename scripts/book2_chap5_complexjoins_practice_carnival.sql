e69c4944caa2d428fb5f69a78d601860cbe9dd8d
Jennifer Schreiner <jennifer.rebecca.schreiner@gmail.com>
2023-10-12 17:37:13 -0500

    weekly update 10-12

--- scripts/book2_chap5_complexjoins_practice_carnival.sql
+++ scripts/book2_chap5_complexjoins_practice_carnival.sql
@@ -14,66 +14,35 @@ FROM dealerships d
 GROUP BY d.dealership_id
 ORDER BY dealership; --code corrected afer review with CHATGPT AND team
 
-
 --Practice: Leased Types
+
 --2. Produce a report that determines the most popular vehicle model that is leased.
-SELECT vt.model AS most_popular_model,
-	COUNT(st.sales_type_id) AS total_leased
-FROM salestypes st 
-	LEFT JOIN sales s ON st.sales_type_id = s.sales_type_id
-	LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
-INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
-WHERE st.sales_type_id = 2
+SELECT vt.model AS most_popular_model, 
+	COUNT(s.sales_type_id) AS total_leased
+FROM sales s 
+	JOIN vehicles v ON s.vehicle_id = v.vehicle_id
+	JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
+WHERE s.sales_type_id = 2
 GROUP BY vt.model
 ORDER BY total_leased DESC
 LIMIT 1;
---join to salestypes isn't needed because there is a sales_type_id field in sales table
-
 --The most popular leased vehicle is the Maxima, with 294 leases
---NOTE: could this be done with a subquery in from, for a MAX on a COUNT?
 
 --Practice: Who Sold What
---What is the most popular vehicle make in terms of number of sales?
 
---Q. Does sales include both purchases and leases?
+--What is the most popular vehicle make in terms of number of sales?
+--NOTE: If "sales" includes both purchases and leases:
 SELECT 	vt.make AS most_popular_make,
 		COUNT(*) AS total_sales
 FROM sales s
-LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
-INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
+	JOIN vehicles v ON s.vehicle_id = v.vehicle_id
+	JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
 GROUP BY vt.make
 ORDER BY total_sales DESC
 LIMIT 1;
---Nissan is the most popular vehicle with 1360 purchase sales
+--Nissan is the most popular vehicle with 1360 total sales
 
---Chapter 14 NOTE: revise this code to use a rank windows function CTE & create a view
---What is the most popular vehicle make in terms of number of sales?
-SELECT 	vt.make AS most_popular_make,
-		COUNT(*) AS total_sales
-FROM sales s
-LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
-INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
-WHERE s.sales_type_id = 1
-GROUP BY vt.make
-ORDER BY total_sales DESC
-LIMIT 1;--if sales means just purchased vehicles
---Nissan is the most popular vehicle with 679 purchase sales
-
---Chapter 14 NOTE: revise this code to use the view from previous to pull most popular vehicle by sales
 --2. Which employee type sold the most of that make?
-SELECT  et.employee_type_name,
-		COUNT(s.sale_id) AS total_nissan_sales
-FROM
-	employeetypes et
-	INNER JOIN employees e ON et.employee_type_id = e.employee_type_id
-	INNER JOIN sales s ON e.employee_id = s.employee_id
-	LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
-	INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
-WHERE vt.make LIKE 'Nissan' AND s.sales_type_id = 1
-GROUP BY et.employee_type_name
-ORDER BY total_nissan_sales DESC
-LIMIT 1;	--Customer Service employee type sold the most Nissans (117) where sales are puchase only
-
 SELECT  et.employee_type_name,
 		COUNT(s.sale_id) AS total_nissan_sales
 FROM
@@ -87,33 +56,18 @@ GROUP BY et.employee_type_name
 ORDER BY total_nissan_sales DESC
 LIMIT 1;	--Customer Service employee type also sold the most Nissans (507) if sales include purchase & leases
 
---fix code using a CTE or a subquery to combine queries
-
---Asked ChatGPT to combine the two queries, it chose to use a CTE and a subquery:
-WITH VehicleSales AS (SELECT vt.make AS most_popular_make,
-          				COUNT(*) AS total_sales
-    					FROM sales s 
-   						LEFT JOIN  salestypes st ON s.sales_type_id = st.sales_type_id
-    					LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
-    					INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
-   						WHERE st.sales_type_id = 1
-   						GROUP BY vt.make
-   						ORDER BY total_sales DESC
-    					LIMIT 1
-						)
-
-SELECT et.employee_type_name,
-       COUNT(s.sale_id) AS total_nissan_sales
-FROM employeetypes et
-INNER JOIN employees e ON et.employee_type_id = e.employee_type_id
-INNER JOIN sales s ON e.employee_id = s.employee_id
-LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
-INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
-WHERE vt.make = (SELECT most_popular_make FROM VehicleSales)
-      AND s.sales_type_id = 1 --change if sales include leased and purchased vehicles
+SELECT  et.employee_type_name,
+		COUNT(s.sale_id) AS total_nissan_sales
+FROM
+	employeetypes et
+	INNER JOIN employees e ON et.employee_type_id = e.employee_type_id
+	INNER JOIN sales s ON e.employee_id = s.employee_id
+	LEFT JOIN vehicles v ON s.vehicle_id = v.vehicle_id
+	INNER JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
+WHERE vt.make LIKE 'Nissan' AND s.sales_type_id = 1
 GROUP BY et.employee_type_name
 ORDER BY total_nissan_sales DESC
-LIMIT 1;
+LIMIT 1;	--Customer Service employee type sold the most Nissans (117) where sales are puchase only
 
 --MORE practice questions FROM CHATGPT:
 --1. Employee Performance Evaluation:
--- scripts/book3_GroupProject_carnival.sql
+++ scripts/book3_GroupProject_carnival.sql
@@ -306,7 +306,232 @@ $$ language plpgsql;
 
 
 
+--William's team's solutions:
 
+/*
+Creating Carnival Reports
+As Carnival grows, we have been asked to help solve two issues:
+​
+It has become more and more difficult for the accounting department to keep track of the all the records in the sales table and how much money came in from each sale.
+​
+HR currently has an overflowing filing cabinet with files on each employee. There's additional files for each dealership. Sorting through all these files when new employees join Carnival and current employees leave is a process that needs to be streamlined. All employees that start at Carnival are required to work shifts at at least two dealerships.
+​
+Goals
+Using CREATE to add new tables
+Using triggers
+Using stored procedures
+Using transactions
+Practice
+*/
+​
+​
+/*Provide a way for the accounting team to track all financial transactions by creating a new table called Accounts Receivable. The table should have the following columns: credit_amount, debit_amount, date_received as well as a PK and a FK to associate a sale with each transaction.*/
+​
+CREATE TABLE accounts_receivable (
+    ar_id SERIAL PRIMARY KEY,
+    credit_amount NUMERIC(15,2),
+    debit_amount NUMERIC(15,2),
+    date_received DATE DEFAULT CURRENT_DATE,
+    sale_id INT,
+	FOREIGN KEY(sale_id) REFERENCES sales(sale_id)
+);
+​
+/*Set up a trigger on the Sales table. When a new row is added, add a new record to the Accounts Receivable table with the deposit as credit_amount, the timestamp as date_received and the appropriate sale_id.*/
+​
+CREATE OR REPLACE FUNCTION new_sale()
+RETURNS TRIGGER
+LANGUAGE plpgsql AS
+$$
+BEGIN
+	
+	INSERT INTO accounts_receivable (credit_amount, sale_id)
+	VALUES (NEW.deposit, NEW.sale_id)
+	; 
+​
+	RETURN NEW;
+END
+$$
+;
+​
+CREATE OR REPLACE TRIGGER new_sale_update_ar
+AFTER INSERT
+ON sales
+FOR EACH ROW
+EXECUTE PROCEDURE new_sale()
+;
+​
+-- Testing --
+​
+INSERT INTO sales (sales_type_id, 
+				   vehicle_id, 
+				   employee_id, 
+				   customer_id,
+				   dealership_id, 
+				   price, 
+				   deposit, 
+				   purchase_date, 
+				   pickup_date, 
+				   invoice_number, 
+				   payment_method, 
+				   sale_returned)
+VALUES (
+	2, 69, 34, 44, 4, 23442, 3224, current_date, current_date , '2232323233', 'mastercard', false);
+​
+SELECT * FROM accounts_receivable;
+​
+/*Set up a trigger on the Sales table for when the sale_returned flag is updated. Add a new row to the Accounts Receivable table with the deposit as debit_amount, the timestamp as date_received, etc.*/
+​
+CREATE OR REPLACE FUNCTION sale_return()
+RETURNS TRIGGER
+LANGUAGE plpgsql AS
+$$
+BEGIN
+	 
+	 UPDATE accounts_receivable
+	 SET debit_amount = sales.deposit
+	 FROM sales
+	 WHERE accounts_receivable.sale_id = sales.sale_id;
+	 
+	 RETURN NEW;
+​
+END
+$$
+;
+​
+CREATE OR REPLACE TRIGGER returning_sale
+AFTER UPDATE
+ON sales
+FOR EACH ROW
+EXECUTE PROCEDURE sale_return()
+;
+​
+-- Testing --
+​
+UPDATE sales
+SET sale_returned = 'true'
+WHERE sale_id = 5029;
+​
+SELECT * FROM sales ORDER BY sale_id DESC;
+​
+SELECT * FROM accounts_receivable;
+​
+/*Create a stored procedure with a transaction to handle hiring a new employee. Add a new record for the employee in the Employees table and add a record to the Dealershipemployees table for the two dealerships the new employee will start at.*/
+​
+CREATE OR REPLACE PROCEDURE new_hire(first_name varchar (30), 
+									 last_name varchar (30),
+									 email_address varchar (50) DEFAULT NULL,
+									 phone varchar (12) DEFAULT NULL,
+									 employee_type_id integer DEFAULT NULL)
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+​
+	employeeId int;
+	dealership1 int;
+	dealership2 int;
+	
+BEGIN	
+​
+	SELECT dealership_id 
+	FROM dealerships 
+	ORDER BY RANDOM() LIMIT 1 
+	INTO dealership1;
+	
+	SELECT dealership_id 
+	FROM dealerships 
+	WHERE dealership_id <> dealership1
+	ORDER BY RANDOM() LIMIT 1 
+	INTO dealership2;
+	
+	BEGIN	
+​
+		INSERT INTO employees (first_name, last_name, email_address, phone, employee_type_id)
+		VALUES (first_name, last_name, email_address, phone, employee_type_id)
+		RETURNING employee_id INTO employeeId;
+	
+		COMMIT;
+​
+		INSERT INTO dealershipemployees (dealership_id, employee_id)
+		VALUES (dealership1, employeeId), (dealership2, employeeId);
+	
+		COMMIT;
+​
+	END;
+END	
+$$
+;
+​
+-- Executing procedure -- 
+CALL new_hire('John', 'Doe');
+​
+-- Testing --
+SELECT * 
+FROM employees 
+	LEFT JOIN dealershipemployees USING (employee_id)
+WHERE first_name = 'John' AND last_name = 'Doe'
+​
+​
+-- Group Solution -- 
+​
+CREATE OR REPLACE PROCEDURE add_employee_to_dealerships()
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+  NewEmployeeId integer;
+  
+BEGIN
+    INSERT INTO employees (first_name, last_name, email_address, phone, employee_type_id)
+    VALUES ('John', 'Doe', 'johndoe@example.com', '123-456-7890', 1)
+		RETURNING employee_id INTO NewEmployeeId;
+​
+COMMIT;
+​
+	    INSERT INTO dealershipemployees (dealership_id, employee_id)
+    	VALUES (1, NewEmployeeId),
+    		   (2, NewEmployeeId);
+		
+COMMIT;
+​
+END;
+$$;
+​
+-- Testing --
+​
+CALL add_employee_to_dealerships();
+​
+SELECT * FROM dealershipemployees d ORDER BY dealership_employee_id DESC;
+SELECT * FROM employees e ORDER BY e.employee_id DESC;
+​
+​
+/*Create a stored procedure with a transaction to handle an employee leaving. The employee record is removed and all records associating the employee with dealerships must also be removed.*/
+​
+​
+CREATE OR REPLACE PROCEDURE remove_employee(IN EmployeeId INT)
+LANGUAGE plpgsql
+AS $$
+  
+BEGIN
+    DELETE FROM dealershipemployees de WHERE de.employee_id = EmployeeId;
+   
+    DELETE FROM accounts_receivable WHERE sale_id IN (SELECT sale_id FROM sales WHERE employee_id = EmployeeId);
+​
+    DELETE FROM sales s WHERE s.employee_id = EmployeeId;
+    
+    DELETE FROM employees e WHERE e.employee_id = EmployeeId;
+    
+COMMIT;
+​
+END;
+$$;
+​
+-- Executing Procedure --
+CALL remove_employee(1010);
+​
+-- Validating --
+SELECT * FROM dealershipemployees d ORDER BY dealership_employee_id DESC;
+SELECT * FROM employees e ORDER BY e.employee_id DESC;
+SELECT * FROM sales s ORDER BY s.sale_id DESC;
 
 
 
